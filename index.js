7303c74eeb5e309f4f669b2a04fdb290f8fbcc52
Maximiliano <maxigomez.udaondo@gmail.com>
2025-02-18 18:26:55 -0300

    qqqqtttttttttt

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ import cors from 'cors';
 import NewsAPI from 'newsapi';
 
 const app = express();
-const newsapi = new NewsAPI('31bca8c94a954f229cc12bdde714cedd', { 
-    corsProxyUrl: 'https://cors-anywhere.herokuapp.com/'} ); // Usar la clave desde variables de entorno
+const newsapi = new NewsAPI('31bca8c94a954f229cc12bdde714cedd'); // Usar la clave desde variables de entorno
 
 // Configurar CORS
 app.use(cors({
