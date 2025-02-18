3ac0d0c382ead4d6cc3d6acf0ecd76ea82e49156
Maximiliano <maxigomez.udaondo@gmail.com>
2025-02-18 13:59:54 -0300

    newsAPI

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import NewsAPI from 'newsapi';
 
 import noticiasRouter from './services/noticias.services.js'
 
@@ -19,6 +20,25 @@ app.use(noticiasRouter);
 // Ruta para el home
 app.get('/', (req, res) => {
     res.send('Servidor funcionando');
+});
+
+const newsapi = new NewsAPI('31bca8c94a954f229cc12bdde714cedd');
+// To query /v2/top-headlines
+// All options passed to topHeadlines are optional, but you need to include at least one of them
+newsapi.v2.topHeadlines({
+  sources: 'bbc-news,the-verge',
+  q: 'bitcoin',
+  category: 'business',
+  language: 'en',
+  country: 'us'
+}).then(response => {
+  console.log(response);
+  /*
+    {
+      status: "ok",
+      articles: [...]
+    }
+  */
 });
 
 app.listen(PORT, () => {
