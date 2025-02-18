de18fb19758d9da89c14841af52ba045b1bd9c50
Maximiliano <maxigomez.udaondo@gmail.com>
2025-02-18 13:30:05 -0300

    cors/noticias

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 // Configurar CORS
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://financial-arg.vercel.app']
+    origin: ['http://localhost:3000', 'https://financial-arg.vercel.app/noticias']
 }));
 
 // Ruta para el home
